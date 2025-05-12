2e20de666c5f0e9bcdd79be495bb2690dbbec517
Berru Hanedar <berruhanedar@icloud.com>
2025-05-13 00:22:30 +0300

    implement custom handler types for login and welcome pages

--- main.go
+++ main.go
@@ -5,6 +5,17 @@ import (
 	"net/http"
 )
 
+type login int
+type welcome int
+
+func (l login) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "on login page")
+}
+
+func (wl welcome) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "on welcome page")
+}
+
 /* type Handler interface {
 	ServeHTTP(ResponseWriter, *Request)
 } */
@@ -40,8 +51,14 @@ func myWelcome(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/login", myLogin)
-	http.HandleFunc("/welcome", myWelcome)
+	// http.HandleFunc("/login", myLogin)
+	// http.HandleFunc("/welcome", myWelcome)
+	// http.Handle("/login", http.HandlerFunc(myLogin))
+	// http.Handle("/welcome", http.HandlerFunc(myWelcome))
+	var i login
+	var j welcome
+	http.Handle("/login", i)
+	http.Handle("/welcome", j)
 	fmt.Println("Listening on port 8080...")
 	http.ListenAndServe("localhost:8080", nil)
 }
