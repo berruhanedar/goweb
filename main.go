e36e330ff3ff1d15d6c173209540162ef19c8bf6
Berru Hanedar <berruhanedar@icloud.com>
2025-05-13 00:06:01 +0300

    create basic HTTP server with /login and /welcome endpoints

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+/* type Handler interface {
+	ServeHTTP(ResponseWriter, *Request)
+} */
+
+func myLogin(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `	
+		<html>
+		  <head>
+		    Hi
+		  </head>
+	      <body>
+		  <h1>
+		   Please enter your username and password
+		  </h1>
+	      </body>
+		<html>	
+	`)
+}
+
+func myWelcome(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `	
+		<html>
+		  <head>
+		    Hi
+		  </head>
+	      <body>
+		  <h1>
+		   Welcome
+		  </h1>
+	      </body>
+		<html>	
+	`)
+}
 
 func main() {
+	http.HandleFunc("/login", myLogin)
+	http.HandleFunc("/welcome", myWelcome)
+	fmt.Println("Listening on port 8080...")
 	http.ListenAndServe("localhost:8080", nil)
 }
