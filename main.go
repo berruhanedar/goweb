31bdd140995c073951d3e12e81b9989eaa91855d
Berru Hanedar <berruhanedar@icloud.com>
2025-05-13 00:36:42 +0300

    enhance login and welcome handlers with request method handling and logging

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-type login int
+/* type login int
 type welcome int
 
 func (l login) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -14,51 +14,37 @@ func (l login) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func (wl welcome) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(w, "on welcome page")
-}
+} */
 
 /* type Handler interface {
 	ServeHTTP(ResponseWriter, *Request)
 } */
 
 func myLogin(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, `	
-		<html>
-		  <head>
-		    Hi
-		  </head>
-	      <body>
-		  <h1>
-		   Please enter your username and password
-		  </h1>
-	      </body>
-		<html>	
-	`)
+	switch r.Method {
+	case "GET":
+		fmt.Fprint(w, "Using GET")
+	case "POST":
+		fmt.Fprintln(w, "Using POST")
+	}
+
+	fmt.Println(w, "on login page")
 }
 
 func myWelcome(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, `	
-		<html>
-		  <head>
-		    Hi
-		  </head>
-	      <body>
-		  <h1>
-		   Welcome
-		  </h1>
-	      </body>
-		<html>	
-	`)
+	fmt.Fprintf(w, "My Request : %+v\n", r)
+	fmt.Println(w, "on login page")
 }
 
 func main() {
-	// http.HandleFunc("/login", myLogin)
-	// http.HandleFunc("/welcome", myWelcome)
+	http.HandleFunc("/login", myLogin)
+	http.HandleFunc("/welcome", myWelcome)
 	// http.Handle("/login", http.HandlerFunc(myLogin))
 	// http.Handle("/welcome", http.HandlerFunc(myWelcome))
-	var i login
-	var j welcome
-	http.Handle("/login", i)
-	http.Handle("/welcome", j)
+	// var i login
+	// var j welcome
+	// http.Handle("/login", i)
+	// http.Handle("/welcome", j)
 	fmt.Println("Listening on port 8080...")
 	http.ListenAndServe("localhost:8080", nil)
 }
