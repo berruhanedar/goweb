7df9c8a6fac2304d8bed2bdd6ebeb83536bb7ee4
Berru Hanedar <berruhanedar@icloud.com>
2025-05-13 01:07:43 +0300

    set up Echo server for handling HTTP requests

--- go.mod
+++ go.mod
@@ -7,7 +7,19 @@ require (
 	github.com/codegangsta/envy v0.0.0-20141216192214-4b78388c8ce4 // indirect
 	github.com/codegangsta/gin v0.0.0-20230218063734-2c98d96c9244 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.7 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/echo/v4 v4.13.3 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-shellwords v1.0.12 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/urfave/cli v1.22.16 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,50 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
-/* type login int
-type welcome int
-
-func (l login) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(w, "on login page")
-}
-
-func (wl welcome) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(w, "on welcome page")
-} */
-
-/* type Handler interface {
-	ServeHTTP(ResponseWriter, *Request)
-} */
-
-func myLogin(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		fmt.Fprint(w, "Using GET")
-	case "POST":
-		fmt.Fprintln(w, "Using POST")
-	}
-
-	fmt.Println(w, "on login page")
-}
-
-func myWelcome(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "My Request : %+v\n", r)
-	fmt.Println(w, "on login page")
-}
-
 func main() {
-	http.HandleFunc("/login", myLogin)
-	http.HandleFunc("/welcome", myWelcome)
-	// http.Handle("/login", http.HandlerFunc(myLogin))
-	// http.Handle("/welcome", http.HandlerFunc(myWelcome))
-	// var i login
-	// var j welcome
-	// http.Handle("/login", i)
-	// http.Handle("/welcome", j)
-	fmt.Println("Listening on port 8080...")
-	http.ListenAndServe("localhost:8080", nil)
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Well , hello there !!!")
+	})
+
+	e.Logger.Print("Listening on port 8080")
+	e.Logger.Fatal(e.Start(":8080"))
 }
